7738321ff8f56422e485da605ce00c7a63dcfc15
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-02-03 20:33:36 +0100

    redoing some output
    why am i alone?

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ i = 0  # data readings counter
 def create_csv(data_file):  # creating csv file
     with open(data_file, 'w') as f:  # create csv file and set up logging
         writer = csv.writer(f)  # set up writer
-        header = ("RowNum", " Date/time", " Temperature", " Humidity", " Pressure", " Movement")  # write first line (data type)
+        header = ("RowNum", " Date/time", " Temperature", " Humidity", " Pressure", " Motion")  # write first line (data type)
         writer.writerow(header)  # write header to csv file
         print("Creating data.csv file...")  # debug
 
@@ -69,10 +69,10 @@ while currentTime < startTime + timedelta(minutes=175):  # run for 175 minutes (
         temp = sense.get_temperature()  # get temperature data from sense hat(used to display temperature to LED matrix)
         sense.show_message("Hello, temperature is %d" % temp, text_colour=blue, back_colour=black)  # print temperature to LED matrix
 
-    if pir == 1:  # run if movement is detected
-        print("Movement detected! Collecting data more frequently...")  # debug
+    if pir == 1:  # run if motion is detected
+        print("Motion detected! Collecting data more frequently...")  # debug
         read_data(data_file)  # collect data from all sensors
-        sense.show_message("Movement detected!")  # print "Movement detected!" to LED matrix
+        sense.show_message("Motion detected!")  # print "Motion detected!" to LED matrix
         sleep(1)  # pause for one second
         for k in range(9):  # run 9 times (10 with data collection above)
             read_data(data_file)  # collect data from all sensors
