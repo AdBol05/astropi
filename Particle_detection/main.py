6e04290222f3bcf4ef31a8bb4f35a3e63cd07ad1
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-02-06 16:00:47 +0100

    Update main.py

--- Particle_detection/main.py
+++ Particle_detection/main.py
@@ -1,1 +1,60 @@
+importing necessary libraries
+import csv
+from sense_hat import SenseHat
+from pathlib import Path
+from datetime import datetime, timedelta
+from time import time, sleep
+from picamera import PiCamera
+from orbit import ISS
+from skyfield.api import load
 
+#mainStartTime = time()
+startTime = datetime.now()  # get program start time
+
+counter = 1  # image counter
+i = 0  # readings counter
+
+def create_csv(data_file):  # creating csv file
+    with open(data_file, 'w', buffering=1) as f:  # create csv file and set up logging
+        writer = csv.writer(f)  # set up writer
+        header = ("RowNum", "date", "coordinates", "magnetometer")  # write first line (data type)
+        writer.writerow(header)  # write header to csv file
+        print("Creating data.csv file...")  # debug
+
+
+def add_csv_data(data_file, data):  # writing data to csv file
+    with open(data_file, 'a', buffering=1) as f:  # open csv file
+        writer = csv.writer(f)  # set up writer
+        writer.writerow(data)  # write data row to scv file
+        print("Writing data to .csv file...")  # debug
+
+
+def read_data(data_file):  # data collection
+    global i  # readings counter as a global variable
+    t = load.timescale().now()
+    position = ISS.at(t)
+    location = position.subpoint()
+    i = i + 1  # increase readings counter by one
+    row = (i, datetime.now(), location, sense.get_compass_raw())  # assign data to row
+    #print("sensing data...")  # debug
+    add_csv_data(data_file, row)  # write row to csv file
+
+
+base_folder = Path(__file__).parent.resolve()  # determine working directory
+data_file = base_folder / 'data.csv'  # set data.csv file name and location
+sense = SenseHat() # set up sense hat
+camera = PiCamera()  # set up camera
+sense.set_imu_config(True, False, False)  # configure imu
+camera.resolution = (1296, 972)  # set camera resolution
+print("running...")  # debug
+create_csv(data_file)  # create data.csv file
+
+currentTime = datetime.now()
+#10 seconds loop with main code
+while (currentTime < startTime + timedelta(minutes=10)):
+    camera.capture(f"{base_folder}/image_{counter}.jpg")
+    print("took a picture")
+    read_data(data_file)
+    sleep(2)
+    counter += 1
+    currentTime = datetime.now()
