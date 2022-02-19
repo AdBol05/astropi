0ddc42e1a0e140e4cb604811c7024941608701b9
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-02-19 18:40:01 +0100

    Update main.py

--- Lightning_detection/main.py
+++ Lightning_detection/main.py
@@ -7,17 +7,20 @@ from time import time, sleep
 from picamera import PiCamera
 from orbit import ISS
 from skyfield.api import load
+import os
 
 startTime = datetime.now()  # get program start time
 
 counter = 10000  # image counter (start from 10000 for better naming scheme)
 i = 0  # readings counter
 storage = 7400000  # used storage space (headroom for script and csv file)
+image_size = 0
+delete_counter = 0
 
-vals_x = []
-vals_y = []
-vals_z = []
-spike = False
+vals_x = []  # value list from magnetometer (x axis)
+vals_y = []  # y axis
+vals_z = []  # z axis
+spike = False  # detected spike
 
 def create_csv(data_file):  # creating csv file
     with open(data_file, 'w', buffering=1) as f:  # create csv file and set up logging
@@ -58,29 +61,43 @@ currentTime = datetime.now()  # get current time before loop start
 while (currentTime < startTime + timedelta(minutes=175) and storage < 3000000000):  # run for 175 minutes (3 hours - 5 minutes) or until storage is full
     read_data(data_file)  # gather data
     for k in range(10):
-        #camera.capture(f"{base_folder}/img_{counter}.jpg")  # capture camera and save the image
-        counter += 1
-        compass = sense.get_compass_raw()
-        vals_x.append(abs(float("{x}".format(**compass))))
-        vals_y.append(abs(float("{y}".format(**compass))))
-        vals_z.append(abs(float("{z}".format(**compass))))
-        sleep(1)
+        camera.capture(f"{base_folder}/img_{counter}.jpg")  # capture camera and save the image
+        image_size = image_size + os.path.getsize(base_folder/f'img_{counter}.jpg')  #get image counter
+        counter = counter + 1  # add one to image counter
+        compass = sense.get_compass_raw()  # get data from magnetometer (compass)
+        vals_x.append(abs(float("{x}".format(**compass))))  # assign data from magnetometer (x axis) to list vals_x
+        vals_y.append(abs(float("{y}".format(**compass))))  # assign data from magnetometer (y axis) to list vals_y
+        vals_z.append(abs(float("{z}".format(**compass))))  # assign data from magnetometer (z axis) to list vals_z
+        sleep(1)  # wait one second
 
-    avrg_x = sum(vals_x) / len(vals_x)
-    absmax_x = (max(vals_x) + 10) - avrg_x
+    avrg_x = sum(vals_x) / len(vals_x)  # get average value
+    absmax_x = (max(vals_x) + 10) - avrg_x  # get max value plus deviation
     avrg_y = sum(vals_y) / len(vals_y)
     absmax_y = (max(vals_y) + 10) - avrg_y
     avrg_z = sum(vals_z) / len(vals_z)
     absmax_z = (max(vals_z) + 10) - avrg_z
 
-    for j in range(len(vals_x)):
+    for j in range(len(vals_x)):  # compare each pair in list
         if i != 0:
-            if (abs(vals_x[j] - vals_x[j - 1]) > absmax_x) or (abs(vals_y[j] - vals_y[j - 1]) > absmax_y) or (abs(vals_z[j] - vals_z[j - 1]) > absmax_z):
-                print("spike detected")
+            if (abs(vals_x[j] - vals_x[j - 1]) > absmax_x) or (abs(vals_y[j] - vals_y[j - 1]) > absmax_y) or (abs(vals_z[j] - vals_z[j - 1]) > absmax_z):  # if detected large difference between on values next to each other
+                print("spike detected")  # debug
                 spike = True
             else:
                 spike = False
 
+    print(delete_counter)  # debug
+
+    if spike == False:
+        for d in range(10):  # run ten times (10 images)
+            delete_counter = (counter - d) - 1
+            os.remove(f"{base_folder}/img_{delete_counter}.jpg")  #remove unnecessary images
+            print("removing images...")  # debug
+            print(delete_counter)  # debug
+    else:
+        storage = storage + image_size  # add images size to storage counter
+        print("saving images")  #debug
+
+    # debug
     print(vals_x)
     print(vals_y)
     print(vals_z)
@@ -90,6 +107,7 @@ while (currentTime < startTime + timedelta(minutes=175) and storage < 3000000000
     vals_y.clear()
     vals_z.clear()
     spike = False
+    image_size = 0
 
     currentTime = datetime.now()  # update current time
-print("Program ended. Timed out or ran out of storage.")
+print("Program ended. Timed out or ran out of storage.")  #debug
