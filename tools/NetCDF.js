f37ecf071de3de4f62d62e5877d8a59c13238b80
AdBol05 <adam.b@volny.cz>
2022-12-13 18:21:16 +0100

    WHYYYYYYY

--- tools/NetCDF.js
+++ tools/NetCDF.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const { NetCDFReader } = require("netcdfjs");
 
 let args = process.argv.slice(2);
 
 let data = fs.readFileSync(args[0], "utf-8");
+let reader = new NetCDFReader(Buffer.from(data));
 
-console.log(data);
+console.log(reader);
