102339e24d76c225f7b711ec5d198a3edd3ecce7
AdBol05 <adam.b@volny.cz>
2022-12-17 23:41:33 +0100

    fixed output directory

--- tools/NetCDF.js
+++ tools/NetCDF.js
@@ -3,7 +3,7 @@ const jpeg = require('jpeg-js');
 
 let args = process.argv.slice(2);
 
-if(args.length !== 5) {console.log('\x1b[31m%s\x1b[0m',"ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <frame number> <image width in px> <image height in px> <output file path>"); process.exit(9);}
+if(args.length !== 5) {console.log('\x1b[31m%s\x1b[0m',"ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <frame number> <image width in px> <image height in px> <output file name>"); process.exit(9);}
 
 let data = fs.readFileSync(args[0], "utf-8");
 
@@ -27,11 +27,12 @@ const width = args[2];
 const height = args[3];
 
 const jpegData = jpeg.encode({ width, height, data: pixels });
-fs.writeFileSync(args[4], jpegData.data);
+let outpath = "./NCtemp/" + args[4];
+fs.writeFileSync(outpath, jpegData.data);
 
 console.log("========================================================")
 console.log(jpegData);
 
 console.log("========================================================")
-console.log('\x1b[32m%s\x1b[0m',"file written to: " + args[4]);
+console.log('\x1b[32m%s\x1b[0m',"file written to: " + outpath);
 console.log("========================================================");
