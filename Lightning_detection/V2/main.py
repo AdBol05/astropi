0b11eb5e8d4fe5a71ea90c5960f2cf279c0f1e0c
AdBol05 <adam.b@volny.cz>
2022-12-22 22:27:34 +0100

    pls work

--- Lightning_detection/V2/main.py
+++ Lightning_detection/V2/main.py
@@ -41,7 +41,7 @@ def read_data(data_file, compass):  # data collection
     location = position.subpoint()  # get location from position
     mag = sense.get_compass_raw()
     i = i + 1  # increase readings counter by one
-    row = (i, datetime.now(), location, mag.x, mag.y, mag.z)  #! TODO:FIX COORDS AND RAW COMPASS DATA OBJECT (assign data to row)
+    row = (i, datetime.now(), location, mag.get("x"), mag.get("y"), mag.get("z"))  #! TODO:FIX COORDS AND RAW COMPASS DATA OBJECT (assign data to row)
     print("sensing data...")  # debug
     add_csv_data(data_file, row)  # write row to csv file
 
