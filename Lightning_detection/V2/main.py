0c2776ce3933bba2f4124dc820c55b0bc3c92f16
AdBol05 <adam.b@volny.cz>
2022-12-23 22:44:28 +0100

    comments

--- Lightning_detection/V2/main.py
+++ Lightning_detection/V2/main.py
@@ -10,8 +10,8 @@ from orbit import ISS
 from skyfield.api import load
 import os
 
+#* define variables 
 startTime = datetime.now()  # get program start time
-
 counter = 10000  # image counter (start from 10000 for better naming scheme)
 i = 0  # readings counter
 storage = 10000  # used storage space (headroom for script and csv file)
@@ -19,6 +19,7 @@ image_size = 0  # size of image
 delete_counter = 0  #iamge counter used for deletion
 spike = 0  # spike detection (set as not found)
 
+#* define functions
 def create_csv(data_file):  # creating csv file
     with open(data_file, 'w', buffering=1) as f:  # create csv file and set up logging
         header = ("RowNum", "date", "coords-latitude", "coords-longitude", "coords-elevation", "magnetometer-X", "magnetometer-Y", "magnetometer-Z")  # write first line (data type)
@@ -42,30 +43,31 @@ def read_data(data_file, compass):  # data collection
     add_csv_data(data_file, row)  # write row to csv file
     i = i + 1  # increase readings counter by one
 
-#some basic init
+#* set up paths
 base_folder = Path(__file__).parent.resolve()  # determine working directory
+data_file = base_folder / 'output/data.csv'  # set data.csv file name and location
 
-#create output directories if they don't exist
 if not os.path.exists(f"{base_folder}/temp"):  # check if temp directory exists
     os.mkdir(f"{base_folder}/temp")  # directory for temporary files
 if not os.path.exists(f"{base_folder}/output"):  # check if output directory exists
     os.mkdir(f"{base_folder}/output")  # directory for output files
 
-data_file = base_folder / 'output/data.csv'  # set data.csv file name and location
+#* sense hat setup (enable magnetometer)
+sense = SenseHat()
+sense.set_imu_config(True, False, False)
 
-sense = SenseHat() # set up sense hat
-sense.set_imu_config(True, False, False)  # configure imu
-
-camera = PiCamera()  # set up camera
-camera.resolution = (1296, 972)  # set camera resolution
+#* camera setup (output resolution)
+camera = PiCamera()
+camera.resolution = (1296, 972)
 
 
+#* initialization
 print("running...")  # debug
 create_csv(data_file)  # create data.csv file
-
 currentTime = datetime.now()  # get current time before loop start
 
 
+#* Main loop
 while (currentTime < startTime + timedelta(minutes=175) and storage < 3000000000):  # run for 175 minutes (3 hours - 5 minutes) or until storage is full
     for k in range(10):  # run ten times (10 images)
         compass = sense.get_compass_raw()  # get data from magnetometer (compass)
@@ -77,13 +79,14 @@ while (currentTime < startTime + timedelta(minutes=175) and storage < 3000000000
         image = Image.open(f"{base_folder}/temp/img_{counter}.jpg")
         image.save(f"{base_folder}/temp/img_{counter}.jpg", exif=image.info["exif"], quality=100)
         
-        counter = counter + 1  # add one to image counter
-        
         print("took a picture")  # debug
         print(image_size)  # debug
 
+        counter = counter + 1  # add one to image counter
+        
         sleep(1)  # wait one second
 
+    #*process images
     #! TODO: Spike detection + coral classification
     if spike == 0:  # if spike is not detected
         for d in range(9):  # run ten times (10 images)
@@ -96,7 +99,7 @@ while (currentTime < startTime + timedelta(minutes=175) and storage < 3000000000
         storage = storage + image_size  # add images size to storage counter
         print("saving images")  # debug
 
-    #reset variables
+    #* reset variables
     spike = 0
     image_size = 0
 
