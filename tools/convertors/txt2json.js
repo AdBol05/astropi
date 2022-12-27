ac7704689f8878d970de4239f0ed933d9071c828
AdBol05 <adam.b@volny.cz>
2022-12-27 23:20:53 +0100

    object keys

--- tools/convertors/txt2json.js
+++ tools/convertors/txt2json.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
 
 let args = process.argv.slice(2);
-if(args[0] === undefined) {console.log('\x1b[31m%s\x1b[0m',"ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <output file path>\nOutput file path is optional. Output file will be written to the same path as input if not defined otherwise."); process.exit(9);}
+if (args[0] === undefined) { console.log('\x1b[31m%s\x1b[0m', "ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <output file path>\nOutput file path is optional. Output file will be written to the same path as input if not defined otherwise."); process.exit(9); }
 let data = fs.readFileSync(args[0], "utf-8");
 
 data = data.split("\n");
@@ -9,6 +9,10 @@ data = data.split("\n");
 let head = data[0];
 data.shift();
 
-console.log(head);
+/*console.log(head);
 console.log(data[0]);
-console.log(data[1]);
+console.log(data[1]);*/
+
+const keys = head.split(",");
+const output = Object.fromEntries(keys.map(key => [key, 0]));
+console.log(output);
