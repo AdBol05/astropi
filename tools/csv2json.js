70d412e13ff237aef7edb5865adb901989512c5b
AdBol05 <adam.b@volny.cz>
2022-12-27 23:44:34 +0100

    TODO: refactor arrays

--- tools/csv2json.js
+++ tools/csv2json.js
@@ -2,12 +2,12 @@ let fs = require('fs');
 
 let args = process.argv.slice(2);
 if (args[0] === undefined) { console.log('\x1b[31m%s\x1b[0m', "ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <output file path>\nOutput file path is optional. Output file will be written to the same path as input if not defined otherwise."); process.exit(9); }
-let data = fs.readFileSync(args[0], "utf-8");
+let input = fs.readFileSync(args[0], "utf-8");
 
-data = data.split("\n");
+input = input.split("\n");
 
-let head = data[0];
-data.shift();
+let head = input[0];
+input.shift();
 
 /*console.log(head);
 console.log(data[0]);
@@ -15,12 +15,15 @@ console.log(data[1]);*/
 
 const keys = head.split(",");
 
-for (i in data) {
-    let sample = data[i].split(",");
-    console.log(sample);
-}
+let data = [];
 
-//* arrays in object are meda from array of arrays
+for (i in input) {
+    data.push(input[i].split(","));
+    //console.log(sample);
+}
+console.log(data);
+
+//* arrays in object are made from array of arrays
 
 let output = Object.fromEntries(keys.map(key => [key, "data placeholder"]));
 console.log(output);
