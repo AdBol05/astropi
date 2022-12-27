43eb560a093ada00cfd1f48cb96b22acde87c843
AdBol05 <adam.b@volny.cz>
2022-12-27 23:07:47 +0100

    txt2json tool base #2

--- tools/convertors/txt2json.js
+++ tools/convertors/txt2json.js
@@ -4,3 +4,11 @@ let args = process.argv.slice(2);
 if(args[0] === undefined) {console.log('\x1b[31m%s\x1b[0m',"ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <output file path (optional)>"); process.exit(9);}
 let data = fs.readFileSync(args[0], "utf-8");
 
+data = data.split("\n");
+
+let head = data[0];
+data.shift();
+
+console.log(head);
+console.log(data[0]);
+console.log(data[1]);
