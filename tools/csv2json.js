f07a274af827c59f13aa95c640d50c336ed2a186
AdBol05 <adam.b@volny.cz>
2022-12-28 01:37:44 +0100

    looks like it works

--- tools/csv2json.js
+++ tools/csv2json.js
@@ -2,10 +2,13 @@ let fs = require('fs');
 
 let args = process.argv.slice(2);
 if (args[0] === undefined) { console.log('\x1b[31m%s\x1b[0m', "ERROR: invalid input, please enter arguments in folowing format:\n<input file path> <output file path>\nOutput file path is optional. Output file will be written to the same path as input if not defined otherwise."); process.exit(9); }
+
+console.log("reading file from " + args[0]);
 let input = fs.readFileSync(args[0], "utf-8");
 
-input = input.split("\n");
+console.log("processing...");
 
+input = input.split("\n");
 let keys = input[0].split(",");
 input.shift();
 let data = [];
@@ -17,12 +20,11 @@ for (i in input){
     }
 }
 
-console.log(data);
-console.log("--------------------------");
-
 let l = 0;
 let output = Object.fromEntries(keys.map(key => [key, data[l++]]));
 console.log(output);
 
 if(args[1] !== undefined){fs.writeFileSync(args[1], JSON.stringify(output));}
 else{fs.writeFileSync(args[0].replace(".csv", ".json"), JSON.stringify(output));}
+
+console.log("written output to " + args[0].replace(".csv", ".json"));
