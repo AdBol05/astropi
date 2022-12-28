d134b57d2ec280f2cd9b792b150919a101162e5a
AdBol05 <adam.b@volny.cz>
2022-12-28 19:34:47 +0100

    variables

--- Lightning_detection/V2/main.py
+++ Lightning_detection/V2/main.py
@@ -70,7 +70,6 @@ def capture(cam, cnt):  # take a picture and add metadata to it (cam -> camera, 
     
     cam.capture(f"{base_folder}/temp/img_{cnt}.jpg")  # capture camera and save the image
 
-    image_size = image_size + os.path.getsize(base_folder/f'temp/img_{cnt}.jpg')  # get image counter
 
     print("took a picture")  # debug
     print(image_size)  # debug
@@ -94,6 +93,7 @@ while (currentTime < startTime + timedelta(minutes=175) and storage < 3000000000
     for k in range(10):
         read_data(data_file)  # get data from all snsors and write to output file
         capture(camera, counter)  # capture image and add metadata to it
+        image_size = image_size + os.path.getsize(base_folder/f'temp/img_{cnt}.jpg')  # get image counter
         counter = counter + 1  # add one to image counter
         sleep(1)  # wait one second
 
