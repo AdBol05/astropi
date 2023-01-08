f845701675fdba65039fe7e639cbceca02d44e8c
Vadzhta <minarikvojta@gmail.com>
2023-01-08 12:17:44 +0100

    Create main.py

---
+++ Magnetism/main.py
@@ -0,0 +1,51 @@
+import csv
+from sense_hat import SenseHat
+from pathlib import Path
+from time import sleep
+from datetime import datetime, timedelta
+import os
+import threading, queue
+import sys
+
+startTime = datetime.now()
+endTime = startTime + timedelta(minutes=175)
+sense = SenseHat()
+
+path = os.path.dirname(os.path.realpath(__file__))
+dataPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data") #create folder dedicated to generated data
+print( 'PATH: {}'.format( dataPath)) #debug, show data path
+if not os.path.exists( dataPath):
+    os.mkdir( dataPath) #create folder if not there already
+dataFile = os.path.join(dataPath, "data")
+
+
+def csvWriter(file, id):
+    try:
+        with open("{}{}.csv".formmar(str(file),str(id)), "w") as f:
+            writer = csv.writer(f)
+            writer.writerow("index", "time", "compass", "compassRawX", "compassRawY", "compassRawZ")
+            i = 0
+            while datetime.now() < endTime:
+                task = csvWrite.get()
+                writer.writerow(i, task[0], task[1], task[2].get("x"), task[2].get("y"), task[2].get("z"))
+    except:
+        e = sys.exc_info()#[0]
+        print('Judge thread closed due to exception')
+        print('  E: {}'.format( e))
+
+
+
+csvWrite = queue.Queue()
+
+for i in range(0,2):
+    threading.Thread(target = csvWriter, args = [dataFile, i]).start()
+
+currentTime = datetime.now()
+
+while currentTime<endTime:
+    compass = sense.get_compass()
+    compassRaw = sense.get_compass_raw()
+    currentTime = datetime.now()
+    csvWrite.put([currentTime, compass, compassRaw])
+    sleep(0)
+print("finished")
