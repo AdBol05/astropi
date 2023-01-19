887a5fb841488a3af5e63f04c224830f486c3c39
AdBol05 <adam.b@volny.cz>
2023-01-19 17:18:07 +0100

    testing

--- Lightning_detection/V2/main.py
+++ Lightning_detection/V2/main.py
@@ -1,6 +1,6 @@
 
 #* importing necessary libraries
-from PIL import Image
+# from PIL import Image
 import csv
 from sense_hat import SenseHat
 from pathlib import Path
@@ -14,17 +14,13 @@ from pycoral.adapters import classify
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.dataset import read_label_file
 import os
-
-# ? import threading, queue # hopefully multithreading (1:CSV data, 2:image collection)
+import threading, queue
 
 #* define variables
-i = 0  # readings counter
-spike = 0  # spike detection (set as not found)
-sequence = 10  # number of images/values to get each loop
 startTime = datetime.now()  # get program start time
-counter = 10000  # image counter (start from 10000 for better naming scheme)
-storage = 25000  # used storage space (headroom for script)
-max_storage = 3000000000  #TODO find ou the exact storage limit
+endTime = startTime + timedelta(minutes=175)
+data_storage_limit = 3000000   # Max data.csv file size is 3MB
+image_storage_limit = 2990000000  # Max combined image size is 2.99GB
 
 #* set up paths (resolve all paths and create file structure)
 base_folder = Path(__file__).parent.resolve()  # determine working directory
@@ -49,24 +45,21 @@ def create_csv(data_file):  # creating csv file
         csv.writer(f).writerow(header)  # write header to csv file
 
 
-def read_data(data_file):  # data collection
-    global i  # readings counter as a global variable
+def read_data(data_file, count):  # data collection
     position = ISS.at(load.timescale().now()).subpoint()  # get position from timescale
     mag = sense.get_compass_raw()  # get magnetometer data
-    data = (i, datetime.now(), position.latitude, position.longitude, position.elevation.km, mag.get("x"), mag.get("y"), mag.get("z"))  # assign data to row
+    data = (count, datetime.now(), position.latitude, position.longitude, position.elevation.km, mag.get("x"), mag.get("y"), mag.get("z"))  # assign data to row
 
     with open(data_file, 'a', buffering=1) as f:  # open csv file
         csv.writer(f).writerow(data)  # write data row to scv file
-        print("Written data to .csv file")  # debug
-
-    i += 1  # increase readings counter by one
+        print("Written data to .csv file")  # 175debug
 
 def angle2exif(angle):  # convert raw coords angle to EXIF friendly format
     sign, degrees, minutes, seconds = angle.signed_dms()
     exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return sign < 0, exif_angle
 
-def capture(cam, cnt):  # take a picture and add metadata to it (cam -> camera, cnt -> image counter)
+def capture(cam, count):  # take a picture and add metadata to it (cam -> camera, cnt -> image counter)
     coords = ISS.coordinates()  # get current ISS coordinates
     south, exif_lat = angle2exif(coords.latitude)  # convert coordinates to exif friendly format
     west, exif_long = angle2exif(coords.longitude)
@@ -77,9 +70,9 @@ def capture(cam, cnt):  # take a picture and add metadata to it (cam -> camera, 
     cam.exif_tags['GPS.GPSLongitude'] = exif_long
     cam.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
 
-    cam.capture(f"{temporary_folder}/img_{cnt:03d}.jpg")  # capture camera and save the image
+    cam.capture(f"{temporary_folder}/img_{count}.jpg")  # capture camera and save the image
 
-    print(f"took a picture: {cnt}")  # debug
+    #print(f"took a picture: {count}")  # debug
 
 def move(name, cnt):
     os.replace(f"{temporary_folder}/img_{cnt}.jpg", f"{output_folder}/{name}_{cnt}.jpg")  # move image to output folder
@@ -92,91 +85,78 @@ sense.set_imu_config(True, False, False)
 #* camera setup (set iamge resolution and zoom)
 camera = PiCamera()
 camera.resolution = (1296, 972)
-camera.zoom = (0.20, 0.155, 0.80, 0.845) #TODO: fix image crop
+#TODO: fix image crop camera.zoom = (0.20, 0.155, 0.80, 0.845) 
 
-#* coral setup
-#TODO: train and implement coral
-"""
-interpreter = make_interpreter(f"{model_file}")  # assign model to interpreter
-interpreter.allocate_tensors()  # set up TPU
-size = common.input_size(interpreter)  # resize image
-"""
+#* define thread functions
+def get_data(startTime, endTime, storage_limit, data_file):
+    storage = 0
+    counter = 0
+    currentTime = datetime.now()  # get current time before loop start
+    while (currentTime < endTime and storage < storage_limit):
+        if counter != 0:  # ignore first iteration
+            storage -= os.path.getsize(data_file)  # subtract old data.csv file size from storage counter
+    
+        read_data(data_file, counter)  # get data from all sensors and write to output file
+        storage += os.path.getsize(data_file)  # add new data.csv file size to storage counter
+        currentTime = datetime.now()  # update time
+        counter += 1
+        print(f"Read data from sensors, used data storage: {storage}")
+        sleep(1)
+
+    print("#------------------------------------------------------------------------------------------------------#")
+    print(f"Data collection thread exited, storage used: {storage}, time elapsed: {datetime.now() - startTime}")
+    print("#------------------------------------------------------------------------------------------------------#")
+
+def get_images(startTime, endTime, storage_limit, camera, counter, sequence, output_folder, temporary_folder):
+    storage = 0
+    spike = 0
+    currentTime = datetime.now()  # get current time before loop start
+    while (currentTime < endTime and storage < storage_limit):
+        for k in range(sequence):
+            capture(camera, counter)
+            counter += 1  # add one to image counter
+            #TODO: save images to output and add size to storage counter
+            print(f"Took image: {counter}, used image storage: {storage}")
+
+        if spike == 0:  # if spike is not detected
+            print(f"#removing images: {counter - sequence + 1} - {counter - 1}")
+            for d in range(sequence):  # run a couple times (save the only last image)
+                delete_counter = (counter - d) - 1  # resovle number of images selected to be deleted
+                if d != (sequence-1):  # delete all images except for the last one
+                    os.remove(f"{temporary_folder}/img_{delete_counter}.jpg")  # remove unnecessary images
+                else:  # save last image
+                    print(f"#saving image: {delete_counter}") # debug
+                    move("img", delete_counter)
+                    storage += os.path.getsize(f"{output_folder}/img_{delete_counter}.jpg")  # add image size to used storage space
+
+        if spike == 1:  # if spike is detected
+            print("#saving all images")  # debug
+            for d in range(sequence):  # save all images
+                move_counter = (counter - d) - 1  # resovle number of images selected to be dmoved
+                move("spike", move_counter)
+                storage += os.path.getsize(f"{output_folder}/spike_{move_counter}.jpg")  # add image size to used storage space
+        
+        currentTime = datetime.now()  # update time
+    
+    print("#------------------------------------------------------------------------------------------------------#")
+    print(f"Image thread exited, storage used: {storage}, time elapsed: {datetime.now() - startTime}")
+    print("#------------------------------------------------------------------------------------------------------#")
+
 
 #* initialization
 create_csv(data_file)  # create data.csv file
-currentTime = datetime.now()  # get current time before loop start
+print("starting threads")
+# define two threads (one for image collection, and one for sensor reading)
+t1 = threading.Thread(target = get_data, args = [startTime, endTime, data_storage_limit, data_file])
+t2 = threading.Thread(target = get_images, args = [startTime, endTime, image_storage_limit , camera, 10000, 10, output_folder, temporary_folder])
 
-#* Main loop
-while (currentTime < startTime + timedelta(minutes=175) and storage < max_storage):  # run for 175 minutes (3 hours - 5 minutes) or until storage is full
-    for k in range(sequence):
-        if counter != 10000:  # ignore for first iteration
-            csv_size_prev = os.path.getsize(data_file)  # get size of data.csv file before data is written
-            storage -= csv_size_prev  # subtract old data.csv file size from storage counter 
+t1.start()  # start threads
+t2.start()  # start threads
 
-        read_data(data_file)  # get data from all snsors and write to output file
-        storage += os.path.getsize(data_file)  # add new data.csv file size of storage counter
-        
-        capture(camera, counter)  # capture image and add metadata to it
-        counter += 1  # add one to image counter
-    
-        time_eplased = datetime.now() - startTime  # get process uptime
-        print(f"used storage: {storage}/{max_storage} time elapsed: {time_eplased}")  # debug
-        print("-----------------------------------------")  # debug
+t1.join()  # wait for threads to finish
+t2.join()  # wait for threads to finish
 
-    #* process images
-    #TODO: Spike detection + coral classification
-
-    #?Ignore this for now
-    """
-    image_file = f"{temporary_folder}/img_{counter}.jpg"  # set image directory
-    image = Image.open(image_file).convert('RGB').resize(size, Image.ANTIALIAS)  # open image
-    common.set_input(interpreter, image)  # set input
-    interpreter.invoke()  # invoke interpreter
-    classes = classify.get_classes(interpreter, top_k=1)  # get classes
-    labels = read_label_file(label_file)  # get labels from label.txt
-
-    for c in classes:
-        print("classifying image...")  # debug
-        print(f'{labels.get(c.id, c.id)} {c.score:.5f}')  # debug
-        if (f'{labels.get(c.id, c.id)}'  == 'lightning' and float(f'{c.score:.5f}') >= 0.3):  # save only images with particles
-            print("classified as lightning, saving...")  # debug
-            os.rename(image_file, f"{temporary_folder}/particle_{counter}.jpg")  # rename image to particle(number of picture).jpg
-            os.replace(f"{temporary_folder}/particle_{counter}.jpg", f"{output_folder}/particle_{counter}.jpg")
-            storage += os.path.getsize(f"{output_folder}/particle_{counter}.jpg")  # add image size to used storage
-            print("saved image, storage used: {storage}")  # debug
-            counter += 1  # increase image counter by one
-        else:
-            print("classified as a blank image, deleting...")  # debug
-            os.remove(image_file)  # delete empty image
-    """
-
-    if spike == 0:  # if spike is not detected
-        print(f"#removing images: {counter - sequence + 1} - {counter - 1}")
-        for d in range(sequence):  # run a couple times (save the only last image)
-            delete_counter = (counter - d) - 1  # resovle number of images selected to be deleted
-            if d != (sequence-1):  # delete all images except for the last one
-                os.remove(f"{temporary_folder}/img_{delete_counter}.jpg")  # remove unnecessary images
-            else:  # save last image
-                print(f"#saving image: {delete_counter}") # debug
-                move("img", delete_counter)
-                storage += os.path.getsize(f"{output_folder}/img_{delete_counter}.jpg")  # add image size to used storage space
-
-    if spike == 1:  # if spike is detected
-        print("#saving all images")  # debug
-        for d in range(sequence):  # save all images
-            move_counter = (counter - d) - 1  # resovle number of images selected to be dmoved
-            move("spike", move_counter)
-            storage += os.path.getsize(f"{output_folder}/spike_{move_counter}.jpg")  # add image size to used storage space
-
-
-    #* reset variables
-    spike = 0
-    print("=========================================") # debug
-
-    currentTime = datetime.now()  # update current time
-
-print(f"#Program ended. All output files are located in {output_folder}")  # debug
-time_eplased = currentTime - startTime
-storage_used = round(storage / (1024 * 1024), 3)
-max_storage_form = round(max_storage / (1024 * 1024), 3)
-print(f"#Time elapsed: {time_eplased}, storage used: {storage_used}/{max_storage_form}MB")
+print("#------------------------------------------------------------------------------------------------------#")
+print(f"Program ended. All output files are located in {output_folder}")  # debug
+print(f"Time elapsed: {datetime.now() - startTime}")
+print("#------------------------------------------------------------------------------------------------------#")
