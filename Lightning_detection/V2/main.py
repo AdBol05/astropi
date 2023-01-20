fd42e5304c49bb83ddad641d2187a714a9ac41e1
AdBol05 <adam.b@volny.cz>
2023-01-20 16:57:38 +0100

    exif elevation

--- Lightning_detection/V2/main.py
+++ Lightning_detection/V2/main.py
@@ -68,8 +68,8 @@ def capture(cam, count):  # take a picture and add metadata to it (cam -> camera
     cam.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
     cam.exif_tags['GPS.GPSLongitude'] = exif_long
     cam.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-    cam.exif_tags['GPS.GPSAltitude'] = int(round(coords.elevation.km, 0))
-    #cam.exif_tags['GPS.GPSAltitude'] = 0
+    cam.exif_tags['GPS.GPSAltitude'] = int(round(coords.elevation.km))
+    cam.exif_tags['GPS.GPSAltitudeRef'] = 0
 
     cam.capture(f"{temporary_folder}/img_{count}.jpg")  # capture camera and save the image
 
