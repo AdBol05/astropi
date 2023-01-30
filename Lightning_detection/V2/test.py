f8a349f19999f38bda2f1b3f74f4049b28647dda
AdBol05 <adam.b@volny.cz>
2023-01-30 16:56:31 +0100

    removed test file

--- Lightning_detection/V2/test.py
+++
@@ -1,9 +0,0 @@
-
-#* importing necessary libraries
-from sense_hat import SenseHat
-from orbit import ISS
-from skyfield.api import load
-import os
-
-print(f"ISS at timescale approach: {ISS.at(load.timescale().now()).subpoint()}")
-print(f"ISS coords approach: {ISS.coordinates()}")
