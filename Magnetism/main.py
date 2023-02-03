8efeeb9bead85fa654e1279222dc9b82dcaf8075
AdBol05 <adam.b@volny.cz>
2023-02-03 12:35:34 +0100

    coords

--- Magnetism/main.py
+++ Magnetism/main.py
@@ -1,5 +1,6 @@
 import csv
 from sense_hat import SenseHat
+from orbit import ISS
 from pathlib import Path
 from time import sleep
 from datetime import datetime, timedelta
@@ -23,11 +24,11 @@ def csvWriter(file, id):
     try:
         with open("{}{}.csv".format(str(file),str(id)), "w") as f:
             writer = csv.writer(f)
-            writer.writerow(["index", "time", "compass", "compassRawX", "compassRawY", "compassRawZ"])
+            writer.writerow(["index", "time", "compass", "compassRawX", "compassRawY", "compassRawZ", "latitude", "longitude", "elevation"])
             i = 0
             while datetime.now() < endTime:
                 task = csvWrite.get()
-                writer.writerow([i, task[0], task[1], task[2].get("x"), task[2].get("y"), task[2].get("z")])
+                writer.writerow([i, task[0], task[1], task[2].get("x"), task[2].get("y"), task[2].get("z"), task[3].latitude, task[3].longitude, task[3].elevation])
     except:
         e = sys.exc_info()#[0]
         print('Writer thread closed due to exception')
@@ -46,6 +47,6 @@ while currentTime<endTime:
     compass = sense.get_compass()
     compassRaw = sense.get_compass_raw()
     currentTime = datetime.now()
-    csvWrite.put([currentTime, compass, compassRaw])
+    csvWrite.put([currentTime, compass, compassRaw, ISS.coordinates()])
     sleep(0)
 print("finished")
