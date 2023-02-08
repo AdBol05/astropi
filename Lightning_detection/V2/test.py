41f6578a864a1990b7c7337a04d525dd6c11e6b9
AdBol05 <adam.b@volny.cz>
2023-02-08 17:03:32 +0100

    test model

--- Lightning_detection/V2/test.py
+++ Lightning_detection/V2/test.py
@@ -1,39 +1,23 @@
-
-#TODO:create and test coral TPU model
-from pathlib import Path
-from orbit import ISS
-from pycoral.adapters import common
-from pycoral.adapters import classify
-from pycoral.utils.edgetpu import make_interpreter
-from pycoral.utils.dataset import read_label_file
 import cv2
+import os
 
-base_folder = Path(__file__).parent.resolve()  # determine working directory
-model_file = base_folder/'lightning.tflite' # set model directory
-label_file = base_folder/'labels.txt' # set label file directory
-dark_image = base_folder/'empty.jpg'
-lightning_image = base_folder/'lightning.jpg'
-input_file = base_folder/'lightning.h264'
+dark_file = "./dark.h264"
+lightning_file = "./lightning.h264"
+dark_folder = "./dark"
+lightning_folder = "./lightning"
 
-"""
-interpreter = make_interpreter(f"{model_file}")  # create an interpreter instance
-interpreter.allocate_tensors()  # set up TPU
-size = common.input_size(interpreter)  # get preffered input image size
-labels = read_label_file(label_file)  # get labels from label.txt
-    
-image = Image.open(dark_image).convert('RGB').resize(size, Image.ANTIALIAS)  # open image
-common.set_input(interpreter, image)  # load model and image to TPU
-interpreter.invoke()  # invoke interpreter
+#* create output and temporary directories if they don't exist
+if not os.path.exists(lightning_folder):
+    print(f"Creating temporary directory in: {lightning_folder}")
+    os.mkdir(lightning_folder)
 
-classes = classify.get_classes(interpreter, top_k=1)  # get classes
+if not os.path.exists(dark_folder):
+    os.mkdir(dark_folder)
+    print(f"Creating output directory in: {dark_folder}")
 
-for c in classes:  # get score of all classes
-    print(f'{labels.get(c.id, c.id)} | {c.score:.5f}')
+print("Processing please wait...")
 
-#?coral(model_file, label_file, dark_image)
-#?coral(model_file, label_file, lightning_image)
-"""
-
+#* parse videos to extract frames
 video1 = cv2.VideoCapture(input_file)
     # Check if video was opened successfully
 if not video1.isOpened():
@@ -45,33 +29,7 @@ while video1.isOpened():
     success, frame = video1.read()
     if not success:
         break
-
+    cv2.imwrite(f"{output_folder}/frame_{i}.jpg", frame)
     i += 1
     # Release the video
-    video1.release()
-    interpreter = make_interpreter(f"{model_file}")  # create an interpreter instance
-    interpreter.allocate_tensors()  # set up TPU
-    size = common.input_size(interpreter)  # get preffered input image size
-    labels = read_label_file(label_file)  # get labels from label.txt
-
-    #* Convert frame to coral-friendly format
-    #?print("converting frame to coral-usable format")  # debug
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # comnvert to RGB
-    frame = cv2.resize(frame, size)  # resize to match the input size of coral model
-    frame = frame.astype('float32') / 255.0  # convert to float in range from 0.0 - 1.0
-
-    #* Classify frame
-    #?print("classifing frame")  # debug
-    common.set_input(interpreter, frame)  # load model and image to TPU
-    interpreter.invoke()  # invoke interpreter
-                    
-    classes = classify.get_classes(interpreter, top_k=1)  # get classes
-
-    for c in classes:  # get score of all classes
-        print(f'{labels.get(c.id, c.id)} | {c.score:.5f}')
-        if(f'{labels.get(c.id, c.id)}'  == 'lightning' and float(f'{c.score:.5f}') >= 0.3):  # if classified as lightning with accuracy higher than 0.3
-            captured = True  # will be set true if at least one of the frames contains lightning
-            print("Lightning detected")  # debug
-                    
-
-video1.release()  # close the video
+video1.release()
--- tools/frame.py
+++ tools/frame.py
@@ -5,21 +5,17 @@ dark_file = "./dark.h264"
 lightning_file = "./lightning.h264"
 dark_folder = "./dark"
 lightning_folder = "./lightning"
+from pycoral.adapters import common
+from pycoral.adapters import classify
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.dataset import read_label_file
 
-#* create output and temporary directories if they don't exist
-if not os.path.exists(lightning_folder):
-    print(f"Creating temporary directory in: {lightning_folder}")
-    os.mkdir(lightning_folder)
-
-if not os.path.exists(dark_folder):
-    os.mkdir(dark_folder)
-    print(f"Creating output directory in: {dark_folder}")
 
 print("Processing please wait...")
 
 def parse(input_file, output_folder):
     #* parse videos to extract frames
-    video1 = cv2.VideoCapture(input_file)
+    video1 = cv2.VideoCapture(lightning_file)
     # Check if video was opened successfully
     if not video1.isOpened():
         raise Exception("Could not open video")
@@ -30,7 +26,7 @@ def parse(input_file, output_folder):
         success, frame = video1.read()
         if not success:
             break
-        cv2.imwrite(f"{output_folder}/frame_{i}.jpg", frame)
+        print(frame)
         i += 1
     # Release the video
     video1.release()
