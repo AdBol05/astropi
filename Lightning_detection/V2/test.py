35e93aede753c9e11107a939534c8df0bca10534
AdBol05 <adam.b@volny.cz>
2023-02-08 15:17:34 +0100

    test model

--- Lightning_detection/V2/test.py
+++ Lightning_detection/V2/test.py
@@ -13,23 +13,18 @@ label_file = base_folder/'labels.txt' # set label file directory
 dark_image = base_folder/'empty.jpg'
 lightning_image = base_folder/'lightning.jpg'
 
-
-#?def classify(model_file, label_file, input):
-
-
-def coral(model_file, label_file, input_file):
-    interpreter = make_interpreter(f"{model_file}")  # create an interpreter instance
-    interpreter.allocate_tensors()  # set up TPU
-    size = common.input_size(interpreter)  # get preffered input image size
-    labels = read_label_file(label_file)  # get labels from label.txt
-    classes = classify.get_classes(interpreter, top_k=1)  # get classes
+interpreter = make_interpreter(f"{model_file}")  # create an interpreter instance
+interpreter.allocate_tensors()  # set up TPU
+size = common.input_size(interpreter)  # get preffered input image size
+labels = read_label_file(label_file)  # get labels from label.txt
+classes = classify.get_classes(interpreter, top_k=1)  # get classes
     
-    image = Image.open(input_file).convert('RGB').resize(size, Image.ANTIALIAS)  # open image
-    common.set_input(interpreter, image)  # load model and image to TPU
-    interpreter.invoke()  # invoke interpreter
+image = Image.open(dark_image).convert('RGB').resize(size, Image.ANTIALIAS)  # open image
+common.set_input(interpreter, image)  # load model and image to TPU
+interpreter.invoke()  # invoke interpreter
 
-    for c in classes:  # get score of all classes
-        print(f'{labels.get(c.id, c.id)} | {c.score:.5f}')
+for c in classes:  # get score of all classes
+    print(f'{labels.get(c.id, c.id)} | {c.score:.5f}')
 
-coral(model_file, label_file, dark_image)
-coral(model_file, label_file, lightning_image)
+#?coral(model_file, label_file, dark_image)
+#?coral(model_file, label_file, lightning_image)
