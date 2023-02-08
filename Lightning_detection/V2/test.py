17c4b36d09ff58dae7f9710a6836dd5b3e619ee6
AdBol05 <adam.b@volny.cz>
2023-02-08 14:53:52 +0100

    test model

--- Lightning_detection/V2/test.py
+++ Lightning_detection/V2/test.py
@@ -13,19 +13,21 @@ label_file = base_folder/'labels.txt' # set label file directory
 dark_image = base_folder/'empty.jpg'
 lightning_image = base_folder/'lightning.jpg'
 
-interpreter = make_interpreter(f"{model_file}")  # create an interpreter instance
-interpreter.allocate_tensors()  # set up TPU
-size = common.input_size(interpreter)  # get preffered input image size
 
-def classify(interpreter, size, label_file, input):
+def classify(model_file, label_file, input):
+
+    interpreter = make_interpreter(f"{model_file}")  # create an interpreter instance
+    interpreter.allocate_tensors()  # set up TPU
+    size = common.input_size(interpreter)  # get preffered input image size
+    labels = read_label_file(label_file)  # get labels from label.txt
+
     image = Image.open(input).convert('RGB').resize(size, Image.ANTIALIAS)  # open image
     common.set_input(interpreter, image)  # load model and image to TPU
     interpreter.invoke()  # invoke interpreter
     classes = classify.get_classes(interpreter, top_k=1)  # get classes
-    labels = read_label_file(label_file)  # get labels from label.txt
 
     for c in classes:  # get score of all classes
         print(f'{labels.get(c.id, c.id)} | {c.score:.5f}')
 
-classify(interpreter, size, label_file, dark_image)
-classify(interpreter, size, label_file, lightning_image)
+classify(model_file, label_file, dark_image)
+classify(model_file, label_file, lightning_image)
