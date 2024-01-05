9140042e59042748bac0be756e6b4f16ac3050ed
AdBol05 <adam.b@volny.cz>
2024-01-05 16:30:57 +0100

    again and again and again

--- velocity/main.py
+++ velocity/main.py
@@ -6,44 +6,39 @@ from datetime import datetime, timedelta
 from time import sleep
 from picamera import PiCamera
 import os
+import threading
 
 startTime = datetime.now()  # get program start time
+endTime = startTime + timedelta(minutes=177)  # run program for 177 minutes (3min headroom from the 3hr limit)
+base_folder = Path(__file__).parent.resolve()  # determine working directory
+output_folder = base_folder/'output'  # set output folder path
+data_file = output_folder/'data.csv'  # set data.csv path
 
-n = 0  # loop counter
 i = 0  # data readings counter
-counter = 10000  # image counter (start from 10000 for better naming scheme)
-storage = 10000  # used storage space (headroom for script and csv file)
+img_counter = 100000  # image counter (start from 10000 for better naming scheme)
+storage_data = 32000000  # used storage space (CSV file)
+storage_img = 2960000000 # used storage space (images)
 
 def create_csv(data_file):  # creating csv file
     with open(data_file, 'w') as f:  # create csv file and set up logging
         writer = csv.writer(f)  # set up writer
-        header = ("RowNum", "Date/time", "Compass", "Compass(raw)", "Accelerometer", "Accelerometer(raw)",  "Gyro", "Gyro(raw)", "Orintation")  # write first line (data type)
+        header = ("RowNum", "DateTime", "accX", "accY", "accZ")  # write first line (data type)
         writer.writerow(header)  # write header to csv file
-        print("Creating data.csv file...")  # debug
+        print(f"Creating data.csv file...\n path: {data_file}")  # debug
 
 
 def add_csv_data(data_file, data):  # writing data to csv file
     with open(data_file, 'a') as f:  # open csv file
         writer = csv.writer(f)  # set up writer
         writer.writerow(data)  # write data row to csv file
-        print("Writing data to .csv file...")  # debug
 
 
 def read_data(data_file):  # data collection
     global i  # readings counter as a global variable
     i = i + 1  # increase readings counter by one
-    
-    compass = sense.get_compass()  # get compass data
-    compass_raw = sense.get_compass_raw()  # get raw compass data
-    accel = sense.get_accelerometer()  # get accelerometer data
-    accel_raw = sense.get_accelerometer_raw()  # get raw accelerometer data
-    gyro = sense.get_gyroscope()  # get gyroscope data
-    gyro_raw = sense.get_gyroscope_raw()  # get raw gyroscope data
-    orientation = sense.get_orientation()  # get orientation
-    
-    row = (i, datetime.now(), compass, compass_raw,  accel_raw, gyro, gyro_raw, orientation)  # assign data to row
-    
-    print("sensing data...")  # debug
+    acc = sense.get_accelerometer_raw()  # get raw accelerometer data
+    data = (i, datetime.now(), round(acc.get("x"), 10), round(acc.get("y"), 10), round(acc.get("z"), 10))  # assign data to row
     add_csv_data(data_file, row)  # write row to csv file
 
 
+create_csv()
