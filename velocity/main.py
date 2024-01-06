9f7fd9c3ea5e4f5b23032f73a817e73db8679290
AdBol05 <adam.b@volny.cz>
2024-01-06 21:15:59 +0100

    i dont need this ...i hope

--- velocity/main.py
+++ velocity/main.py
@@ -18,7 +18,6 @@ data_file = output_folder/'data.csv'  # set data.csv path
 model_file = base_folder/'lightning.tflite' # set model path
 label_file = base_folder/'labels.txt' # set label file path
 
-i = 0  # data readings counter
 img_counter = 100000  # image counter (start from 10000 for better naming scheme)
 storage_data = 32000000  # used storage space (CSV file)
 storage_img = 2960000000 # used storage space (images)
@@ -27,7 +26,7 @@ storage_img = 2960000000 # used storage space (images)
 def create_csv(data_file):  # creating csv file
     with open(data_file, 'w') as f:  # create csv file and set up logging
         writer = csv.writer(f)  # set up writer
-        header = ("RowNum", "DateTime", "accX", "accY", "accZ")  # write first line (data type)
+        header = ("DateTime", "accX", "accY", "accZ")  # write first line (data type)
         writer.writerow(header)  # write header to csv file
         print(f"Creating data.csv file...\n path: {data_file}")  # debug
 
@@ -37,10 +36,8 @@ def add_csv_data(data_file, data):  # appending data to csv file
         writer.writerow(data)  # write data row to csv file
 
 def read_data(data_file):  # data collection
-    global i  # readings counter as a global variable
-    i = i + 1  # increase readings counter by one
     acc = sense.get_accelerometer_raw()  # get raw accelerometer data
-    row = (i, datetime.now(), round(acc.get("x"), 10), round(acc.get("y"), 10), round(acc.get("z"), 10))  # assign data to row
+    row = (datetime.now(), round(acc.get("x"), 10), round(acc.get("y"), 10), round(acc.get("z"), 10))  # assign data to row
     add_csv_data(data_file, row)  # write row to csv file
 
 #* sense hat setup (enable magnetometer)
