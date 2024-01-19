ccd389b936cb090e537cc3aa60b1a4e9cde738c5
AdBol05 <adam.b@volny.cz>
2024-01-20 00:12:52 +0100

    i hate python

--- velocity/main.py
+++ velocity/main.py
@@ -1,5 +1,4 @@
 #* import all required libraries
-import csv
 import os
 import sys
 import threading
@@ -16,54 +15,35 @@ import cv2
 
 #* define variables
 startTime = datetime.now()  # get program start time
-endTime = startTime + timedelta(minutes=177)  # run program for 177 minutes (3min headroom from the 3hr limit)
+endTime = startTime + timedelta(minutes=9, seconds=30)  # run program for 177 minutes (3min headroom from the 3hr limit)
+
 base_folder = Path(__file__).parent.resolve()  # determine working directory
 output_folder = base_folder/'output'  # set output folder path
 temporary_folder = base_folder/'temp'  # set temporary folder path
-data_file = output_folder/'data.csv'  # set data.csv path
+data_file_img = output_folder/'data_img.txt'  # set data.csv path
+data_file_gps = output_folder/'data_gps.txt'  # set data.csv path
 model_file = base_folder/'viewtype.tflite' # set model path
 label_file = base_folder/'viewtype_labels.txt' # set label file path
 
-img_counter = 100000  # image counter (start from 10000 for better naming scheme)
-storage_data = 32000000  # CSV file storage limit
+img_counter = 100  # image counter (start from 10000 for better naming scheme)
+img_limit = 40  # max number of images
+storage_data = 32000000  # text file storage limit
 storage_img = 2960000000 # image storage limit
 
-#* define functions
-def create_csv(data_file):  # creating csv file
-    with open(data_file, 'w') as f:  # create csv file and set up logging
-        writer = csv.writer(f)  # set up writer
-        header = ("DateTime", "accX", "accY", "accZ")  # write first line (data type)
-        writer.writerow(header)  # write header to csv file
-        print(f"Creating data.csv file...\n path: {data_file}")  # debug
-
-def add_csv_data(data_file, data):  # appending data to csv file
-    with open(data_file, 'a') as f:  # open csv file
-        writer = csv.writer(f)  # set up writer
-        writer.writerow(data)  # write data row to csv file
-
-def read_data(data_file):  # data collection
-    acc = sense.get_accelerometer_raw()  # get raw accelerometer data
-    row = (datetime.now(), round(acc.get("x"), 10), round(acc.get("y"), 10), round(acc.get("z"), 10))  # assign data to row
-    add_csv_data(data_file, row)  # write row to csv file
-
 #* define thread functions
-def get_data(startTime, endTime, storage_limit, data_file):  # data collection thread
+def gps_thread(startTime, endTime, storage_limit, data_file):  # data collection thread
     storage = 0  # data.csv file size counter
     print("Started data thread")  # debug
     while (datetime.now() < endTime and storage < storage_limit):  # run until storage or time runs out
-        if storage != 0:  # ignore first iteration
-            storage -= os.path.getsize(data_file)  # subtract old data.csv file size from storage counter
-
-        read_data(data_file)  # get data from all sensors and write to output file
         storage += os.path.getsize(data_file)  # add new data.csv file size to storage counter
         sleep(1)  # wait one second
 
     # debug at the end of thread
     print("#------------------------------------------------------------------------------------------------------#")
-    print(f"Data collection thread exited, storage used: {round(storage/(1024*1024), 2)}/{round(storage_limit/(1024*1024), 2)}MB, time elapsed: {datetime.now() - startTime}")
+    print(f"GPS thread exited, storage used: {round(storage/(1024*1024), 2)}/{round(storage_limit/(1024*1024), 2)}MB, time elapsed: {datetime.now() - startTime}")
     print("#------------------------------------------------------------------------------------------------------#")
 
-def get_images(startTime, endTime, storage_limit, counter, out_dir, temp_dir, model_file, label_file):  # image collection thread
+def img_thread(startTime, endTime, storage_limit, counter, out_dir, temp_dir, model_file, label_file, data_file):  # image collection thread
     storage = 0;  # used storage
     print("Started image thread")  # debug
 
@@ -83,7 +63,7 @@ def get_images(startTime, endTime, storage_limit, counter, out_dir, temp_dir, mo
 
     # debug at the end of thread
     print("#------------------------------------------------------------------------------------------------------#")
-    print(f"Image collection thread exited, storage used: {round(storage/(1024*1024), 2)}/{round(storage_limit/(1024*1024), 2)}MB, time elapsed: {datetime.now() - startTime}")
+    print(f"Image thread exited, storage used: {round(storage/(1024*1024), 2)}/{round(storage_limit/(1024*1024), 2)}MB, time elapsed: {datetime.now() - startTime}")
     print("#------------------------------------------------------------------------------------------------------#")
 
 #* sense hat setup (enable magnetometer)
@@ -93,13 +73,12 @@ sense.set_imu_config(False, False, True)  # enable accelerometer
 #* camera setup (set iamge resolution and frequency)
 camera = PiCamera()
 camera.resolution = (1296, 972)  # max 4056*3040
-camera.framerate = 30
 
 #* initialization
 print("starting threads")  # debug
 # define two threads (one for image collection, and one for sensor reading)
-thread1 = threading.Thread(target = get_data, args = [startTime, endTime, storage_data, data_file])
-thread2 = threading.Thread(target = get_images, args = [startTime, endTime, storage_img, img_counter, output_folder, temporary_folder, model_file, label_file])
+thread1 = threading.Thread(target = gps_thread, args = [startTime, endTime, storage_data, data_file_gps, data_file_gps])
+thread2 = threading.Thread(target = img_thread, args = [startTime, endTime, storage_img, img_counter, output_folder, temporary_folder, model_file, label_file, data_file_img])
 
 #* start threads
 thread1.start()
