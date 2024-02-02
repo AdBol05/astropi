5793291477589db86e63682c1bc3c13d6134c9e2
AdBol05 <adam.b@volny.cz>
2024-02-02 12:44:36 +0100

    TODO: dynamic GSD calculation

--- velocity/main.py
+++ velocity/main.py
@@ -120,7 +120,7 @@ def calculate_mean_distance(coordinates_1, coordinates_2):  # calculate distance
 
 #* camera setup (set iamge resolution)
 camera = PiCamera()
-camera.resolution = (4056, 3040)  # max 4056*3040
+camera.resolution = (1296, 972)  # max 4056*3040
 
 print(camera.resolution)
 
