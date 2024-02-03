a8969152b3dd1de144dca384d84cc0a6e4bf5770
AdBol05 <adam.b@volny.cz>
2024-02-03 21:26:52 +0100

    restructuring

--- velocity/main.py
+++ velocity/main.py
@@ -61,7 +61,7 @@ def img_save(counter):
     print("Saving images...")
     size = 0
     for i in range(img_sequence):  # loop over last images
-        id = counter - i  # resolve image number
+        id = counter - (i + 1)  # resolve image number
         path = f"{output_folder}/img_{id}.jpg"  # resolve image path
         os.replace(f"{temporary_folder}/img_{id}.jpg", path)  # move image to output folder
         size += os.path.getsize(path)  # add image size to counter
@@ -72,7 +72,7 @@ def img_save(counter):
 def img_delete(counter):
     print("Deleting images...")
     for i in range(img_sequence):  # loop over last images
-        id = counter - i  # resolve image number
+        id = counter - (i + 1)  # resolve image number
         path = f"{temporary_folder}/img_{id}.jpg"  # resolve image path
         os.remove(path)  # delete image
         print(f"Removing: {path}")  # debug
