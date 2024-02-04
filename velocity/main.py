17eefe021b6eead5bfff68caea1aab209100acab
AdBol05 <adam.b@volny.cz>
2024-02-04 17:29:42 +0100

    I have no idea what Im doing at this point

--- velocity/main.py
+++ velocity/main.py
@@ -157,7 +157,7 @@ def capture(counter):
         camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
         camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
         camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-        camera.exif_tags['GPS.GPSAltitude'] = altitude #(altitude.numerator, altitude.denominator)
+        camera.exif_tags['GPS.GPSAltitude'] = altitude #TODO fix conversion
 
         path = f"{temporary_folder}/img_{counter}.jpg"
 
