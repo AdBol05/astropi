744b596f810c3eb22e34524e23ddb09e3b775bff
AdBol05 <adam.b@volny.cz>
2024-02-04 12:25:07 +0100

    elevation in exif

--- velocity/main.py
+++ velocity/main.py
@@ -152,7 +152,7 @@ def capture(counter):
         camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
         camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
         camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-        camera.exif_tags['GPS.GPSAltitude'] = coords.elevation.m
+        #camera.exif_tags['GPS.GPSAltitude'] = coords.elevation.m
 
         path = f"{temporary_folder}/img_{counter}.jpg"
 
