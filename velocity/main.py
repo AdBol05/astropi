449a743250f4d22509b27e01d012d02306522edc
AdBol05 <adam.b@volny.cz>
2024-02-09 21:24:06 +0100

    comments

--- velocity/main.py
+++ velocity/main.py
@@ -177,7 +177,6 @@ while(datetime.now() < endTime and (storage_img + storage_txt) <= storage_limit)
     if coral:  # Classify first image only to save time. The images should not change drastically during one iteration
         print("Classifying images...")
         classified = False  # save or delete images based on classifications
-        #! change -> True for testing purposes
 
         #* Open first image and convert it to coral-friendly format -> no need to classify both images since they will not change significantly
         image = PILImage.open(images[0]).convert('RGB').resize(size, PILImage.ANTIALIAS)  # open image
